3a9f5ab911d334a2b573d3f8214957877c3a48c5
Andrew McNeill <andrew@andrewmc.ca>
2022-01-16 22:53:11 -0400

    Recursive array summing function

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const eqArrays = (array1, array2) => {
+
+  if (array1.length !== array2.length) {
+
+    return false;
+
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+
+    if (array1[i] !== array2[i]) {
+
+      return false;
+
+    }
+
+  }
+
+  return true;
+
+};
+
+const sumItems = array => {
+
+  while (!eqArrays(array, array.flat())) {
+    array = array.flat();
+  }
+  return (array.length === 0) ? 0 : array[0] + sumItems(array.slice(1));
+
+};
 
 module.exports = sumItems;
